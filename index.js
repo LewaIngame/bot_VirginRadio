feb3a978f4bb3bf30632273113fd17e24b9df9b6
LewaIngame <36868377+LewaIngame@users.noreply.github.com>
2018-03-28 20:59:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -164,6 +164,27 @@ client.on('message', message => {
     }
 })
 
+client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping") {
+		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
+	} else
+
+	if (command === "say") {
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
+} else
+
 client.on('message', message => {
     var array_msg = message.content.split(' ');
         messages.push(message);
